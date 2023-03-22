7e53a0bdb57e01804c5fb1c0af0c26844fb066c8
Jerry Barrows-Fitzgerald <jbarrowsfitzgerald@gmail.com>
2023-03-21 23:35:56 -0500

    fibonacci sequence tests written

---
+++ series.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    """
+    Calculates the nth position of the fibonacci sequence
+    :param n: the position within the fibonacci sequence being calculated (zero-indexed)
+    :return: integer at the nth position of the fibonacci sequence, or zero (0) if
+    argument provided is invalid.
+    """
+
+    if type(n) != int or n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1)
---
+++ tests/test_series.py
@@ -0,0 +1,41 @@
+from series import fibonacci
+
+
+def test_fibonacci_exists():
+    assert fibonacci
+
+
+def test_fibonacci_one():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_fibonacci_zero():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+
+def test_fibonacci_three():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+
+def test_fibonacci_ten():
+    actual = fibonacci(10)
+    expected = 55
+    assert actual == expected
+
+
+def test_fibonacci_negative():
+    actual = fibonacci(-4)
+    expected = 0
+    assert actual == expected
+
+
+def test_fibonacci_non_numeric():
+    actual = fibonacci("Feel the love!")
+    expected = 0
+    assert actual == expected
